ecf51924e4a9cf12ea38e609d44af98a9c2da133
Dandy Griffin <dan@contagious.nu>
2026-06-18 21:16:57 -0500

    Updates swift-syntax

--- Package.swift
+++ Package.swift
@@ -27,7 +27,7 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.0.0"),
         .package(url: "https://github.com/apple/swift-async-algorithms", from: "1.0.2"),
-        .package(url: "https://github.com/apple/swift-syntax.git", from: "601.0.0"),
+        .package(url: "https://github.com/apple/swift-syntax.git", from: "603.0.0"),
     ],
     targets: [
         .macro(
